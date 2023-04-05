079107df03d1ac4f50594acdc1c4b1881b5f05ff
Rob Thomas <robt84@gmail.com>
2023-04-05 10:46:44 -0500

    colors and styles files

---
+++ assets/Colors.js
@@ -0,0 +1,9 @@
+export const Colors = {
+    primary: '#283F3B',
+    secondary: '#556F44',
+    tertiary: '#659B5E',
+    quaternary: '#95BF74',
+    quinary: '#99DDC8',
+    black: '#000000',
+    white: '#FFFFFF',
+};
